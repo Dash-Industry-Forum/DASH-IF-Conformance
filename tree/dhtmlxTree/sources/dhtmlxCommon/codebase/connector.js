ccfdff5daa24a0e30095c0c8a60a891a20c4d1c5
nomor <m.yusufsener@hotmail.com>
2019-01-31 12:51:26 +0100

    new tree

---
+++ tree/dhtmlxTree/sources/dhtmlxCommon/codebase/connector.js
@@ -0,0 +1,16 @@
+/*
+Product Name: dhtmlxTree 
+Version: 5.1.0 
+Edition: Standard 
+License: content of this file is covered by DHTMLX Commercial or enterpri. Usage outside GPL terms is prohibited. To obtain Commercial or Enterprise license contact sales@dhtmlx.com
+Copyright UAB Dinamenta http://www.dhtmlx.com
+*/
+
+/*
+	@author dhtmlx.com
+	@license GPL, see license.txt
+*/
+
+if (window.dataProcessor && !dataProcessor.prototype.init_original)
+	dataProcessor.prototype.connector_init=true;
+
