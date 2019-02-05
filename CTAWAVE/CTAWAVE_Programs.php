7896fa5cfb81afd76b21b25745f36b4e1e5300b1
niteeshbhat <niteeshbhathm@gmail.com>
2019-02-05 16:48:37 +0100

    additional change to unit tests of WAVE programs

--- CTAWAVE/CTAWAVE_Programs.php
+++ CTAWAVE/CTAWAVE_Programs.php
@@ -50,7 +50,7 @@ function checkCMFHDBaselineConstraints($MediaProfDatabase, $session_dir,$adaptat
         array_push($presentationProfileArray,$presentationProfile );
     }
     if(!(count(array_unique($presentationProfileArray))===1 && array_unique($presentationProfileArray)[0]=="CMFHD"))
-        $errorMsg.="###CTAWAVE check violated: WAVE Content Spec 2018Ed-Section 6.2: 'WAVE CMFHD Baseline Program Shall contain a sequence of one or more CMAF Presentations conforming to CMAF CMFHD profile', violated as not all CMAF presentations conforms to CMFHD. ".$presentationProfile."\n";
+        $errorMsg.="###CTAWAVE check violated: WAVE Content Spec 2018Ed-Section 6.2: 'WAVE CMFHD Baseline Program Shall contain a sequence of one or more CMAF Presentations conforming to CMAF CMFHD profile', violated as not all CMAF presentations conforms to CMFHD. \n";
 
     //WAVE Baseline constraints are already checked, open the log file and check if contains errors and print related error message.
     if(!($opfile = fopen($session_dir. '/' . $CTAspliceConstraitsLog . '.txt', 'r'))){
