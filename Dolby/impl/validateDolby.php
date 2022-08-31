a35ca34d32004a88bd4f58c05ed97a099e1b09b0
Phencys <phencys@phencys.com>
2022-08-31 14:31:39 +0200

    Reimplemented previous (cherry-picked) commit for new Dolby module

--- Dolby/impl/validateDolby.php
+++ Dolby/impl/validateDolby.php
@@ -8,9 +8,10 @@ $period = $mpd_features['Period'][$current_period];
 $adaptationSet = $period['AdaptationSet'][$current_adaptation_set];
 $representation = $adaptationSet['Representation'][$current_representation];
 
-///\TodoRefactor: Re-enalbe this one
-return;
-if ($representation['mimeType'] == 'audio/mp4' )
+$codecs = ($adaptationSet['codecs'] == NULL) ? $representation['codecs'] : $adaptationSet['codecs'];
+$isDolby = (($codecs != NULL) && ((substr($codecs, 0, 4) == "ac-3") || (substr($codecs, 0, 4) == "ec-3") || (substr($codecs, 0, 4) == "ac-4")));
+
+if ($isDolby && $representation['mimeType'] == 'audio/mp4' )
 {
   $atomXml = $session->getRepresentationDir($current_period, $current_adaptation_set, $current_representation);
   $xml = get_DOM("$atomXml/atomInfo.xml", 'atomlist');
