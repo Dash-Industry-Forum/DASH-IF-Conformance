8315920f8f63957ca0785fad445fbbc42da2e600
Daniel Silhavy <daniel.silhavy@fokus.fraunhofer.de>
2022-10-12 17:08:17 +0200

    Fix wrong includes in functional test files

--- functional-tests/dashif/functionalTest.php
+++ functional-tests/dashif/functionalTest.php
@@ -26,8 +26,8 @@ include 'Utils/VisitorCounter.php';  //#Various Session-based functions. No Dire
 //#conditional extra includes for module initialization
 include 'Utils/GlobalVariables.php';
 include 'Utils/PrettyPrint.php';     //#Pretty printing functions for terminal output. No Direct Executable Code.
-include 'Utils/segmentDownload.php'; //#Very large function for downloading data. No Direct Executable Code.
-include 'Utils/segmentValidation.php'; //#Segment validation functions. No Direct Executable Code.
+include 'Utils/segment_download.php'; //#Very large function for downloading data. No Direct Executable Code.
+include 'Utils/segment_validation.php'; //#Segment validation functions. No Direct Executable Code.
 //#Dolby validation functions. Attempt at use of objects. No Direct Executable Code.
 include 'Utils/DolbySegmentValidation.php';
 
@@ -49,7 +49,7 @@ include 'DASH/MPDFeatures.php';
 include 'DASH/validateMPD.php';
 include 'DASH/MPDInfo.php';
 include 'DASH/SchematronIssuesAnalyzer.php';
-include 'DASH/crossValidation.php';
+include 'DASH/cross_validation.php';
 include 'DASH/Representation.php';
 include 'DASH/SegmentURLs.php';
 include 'HLS/HLSProcessing.php';
@@ -71,7 +71,7 @@ final class functionalTest extends TestCase
     public function testThis($stream): void
     {
         $GLOBALS['mpd_url'] = $stream;
-        $enabledModules = ["MPEG-DASH Common", "DASH-IF IOP Conformance"];
+        $enabledModules = ["MPEG-DASH Common", "DASH-IF IOP Conformance", "CMAF"];
         $id = null;
 
         $GLOBALS['logger']->reset($id);
@@ -131,57 +131,5 @@ final class functionalTest extends TestCase
         return $streamsToTest;
     }
 
-    /*
-      public function testMultipleVectorsViaCli(): void
-      {
 
-        $limit = 10;
-        $currentIndex = 0;
-          $content = file_get_contents(
-              "functional-tests/dashjs.json_single");
-          $dbJson = json_decode($content);
-          $streamsToTest = array();
-          foreach($dbJson->items as $item) {
-              foreach($item->submenu as $submenu) {
-                  array_push($streamsToTest, $submenu->url);
-              }
-          }
-
-          foreach ($streamsToTest as $idx => $stream) {
-            if ($limit > 0 && $currentIndex >= $limit){
-              break;
-            }
-
-              $GLOBALS['mpd_url'] = $stream;
-              $enabledModules = ["MPEG-DASH Common", "DASH-IF IOP Conformance"];
-              $id = null;
-
-              $GLOBALS['logger']->reset($id);
-              $GLOBALS['logger']->setSource($GLOBALS['mpd_url']);
-
-              foreach ($GLOBALS['modules'] as $module) {
-                  $enabled = in_array($module->name, $enabledModules);
-
-                  $module->setEnabled($enabled);
-                  if ($module->isEnabled()) {
-                      fwrite(STDERR, "$module->name, ");
-                  }
-              }
-
-              fwrite(STDERR, "Going to parse stream " . $GLOBALS['mpd_url'] . "\n");
-
-              process_MPD(true);//MPD and Segments <==== This currently always leads to FAIL, as session files are moved around
-              //process_MPD(false);//MPD Only
-              //
-              //
-              $currentIndex++;
-          }
-          //update_visitor_counter();
-          $output = $GLOBALS['logger']->asJSON();
-          $this->assertNotNull(
-              $output,
-              "Output is not null"
-          );
-      }
-     */
 }
