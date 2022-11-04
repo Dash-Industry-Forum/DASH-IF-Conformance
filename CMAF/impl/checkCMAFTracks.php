c17059fc1d1b0518348791eee4d0f36b40fff2cc
Phencys <phencys@phencys.com>
2022-11-04 09:17:50 +0100

    Fix for the bug mentioned in https://github.com/Dash-Industry-Forum/DASH-IF-Conformance/issues/603

--- CMAF/impl/checkCMAFTracks.php
+++ CMAF/impl/checkCMAFTracks.php
@@ -36,8 +36,10 @@ $cmaf_cmfc = $return_array[0];
 $cmaf_cmf2 = $return_array[1];
 
 # Store media type for selection set checks later
-$mediaType = $xml->getElementsByTagName('hdlr')->item(0)->getAttribute('handler_type');
-$cmaf_mediaTypes[$current_period][$current_adaptation_set][$current_representation] = $mediaType;
+if($xml->getElementsByTagName('hdlr')->item(0)){
+  $mediaType = $xml->getElementsByTagName('hdlr')->item(0)->getAttribute('handler_type');
+  $cmaf_mediaTypes[$current_period][$current_adaptation_set][$current_representation] = $mediaType;
+}
 
 $adaptationSet = $mpd_features['Period'][$current_period]['AdaptationSet'][$current_adaptation_set];
 
