82af3e9506df4c6127183844a5145199287fd47d
Fritz Heiden <fritz.heiden@mailbox.org>
2022-11-14 12:58:53 +0100

    removed files

--- validate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --rm -ti -w="/var/www/html/Utils" --entrypoint "" --mount type=bind,source="$(pwd)"/Conformance-Frontend,target=/var/www/html/Conformance-Frontend dash-if-conformance:latest php Process_cli.php -s -i "$1"
