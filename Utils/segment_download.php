4977ee8fd009d6a32e194135b13f68a24c9a9ab0
Phencys <phencys@phencys.com>
2022-12-05 12:45:06 +0100

    Removed a very verbose debug message

--- Utils/segment_download.php
+++ Utils/segment_download.php
@@ -70,10 +70,8 @@ function download_data($directory, $array_file, $is_subtitle_rep, $is_dolby)
     # Iterate over $array_file
     for ($index = 0; $index < $segment_count; $index++) {
         $filePath = $array_file[$index];
-        fwrite(STDERR, "Downloading $filePath\n");
         $filename = basename($filePath);
         $file_information = remote_file_size($filePath);
-        print_r($file_information);
         $file_exists = $file_information[0];
         $file_size = ($hls_manifest && $hls_byte_range_size) ? $hls_byte_range_size[$index] + $hls_byte_range_begin[$index] : $file_information[1];
 
