3c7e9de159a84724e99f7a597c9f1eeeef5f54c7
Fritz Heiden <fritz.heiden@fokus.fraunhofer.de>
2022-12-06 08:49:52 +0100

    Converting xml info data in result to json (#621)

--- Conformance-Frontend-JCCP/services/conformance-service.js
+++ Conformance-Frontend-JCCP/services/conformance-service.js
@@ -12,7 +12,11 @@ const ConformanceService = (function () {
     { id: "cta-wave", text: "CTA-WAVE", queryParam: "ctawave" },
     { id: "hbbtv", text: "HbbTV", queryParam: "hbbtv" },
     { id: "dvb", text: "DVB", queryParam: "dvb" },
-    { id: "dash-if-ll", text: "DASH-IF IOP Low Latency", queryParam: "lowlatency" },
+    {
+      id: "dash-if-ll",
+      text: "DASH-IF IOP Low Latency",
+      queryParam: "lowlatency",
+    },
     { id: "dash-if-iop", text: "DASH-IF Interoperability", queryParam: "iop" },
     { id: "dolby", text: "Dolby", queryParam: "dolby" },
   ];
@@ -27,10 +31,76 @@ const ConformanceService = (function () {
     });
     let results = await Net.sendRequest({ method: "GET", uri });
     results = JSON.parse(results);
+    results = convertInfoData(results);
     return results;
 
-
     //return Mock.testResults[0];
+  }
+
+  function convertInfoData(result) {
+    let moduleNames = Object.keys(result.entries).filter(
+      (key) => key !== "Stats" && key !== "verdict"
+    );
+    let modules = moduleNames.map((name) => {
+      let module = result.entries[name];
+      module.name = name;
+      return module;
+    });
+
+    modules.forEach((module) => {
+      let partNames = Object.keys(module).filter(
+        (key) => typeof module[key] === "object" && "test" in module[key]
+      );
+      let parts = partNames.map((partName) => {
+        let part = module[partName];
+        part.name = partName;
+        return part;
+      });
+
+      parts.forEach((part) => {
+        if (!part.info) return;
+        let { info } = part;
+        info.forEach((info) => {
+          let xmlStart = info.split("").findIndex((char) => char === "<");
+          if (xmlStart === -1) return;
+          let xmlEnd = info
+            .split("")
+            .reverse()
+            .findIndex((char) => char === ">");
+          xmlEnd = info.length - xmlEnd;
+          let xmlString = info.substring(xmlStart, xmlEnd);
+          xmlString = `<results>${xmlString}</results>`;
+          let parser = new DOMParser();
+          let xml = parser.parseFromString(xmlString, "text/xml");
+          let xmlTestResults = Array.from(
+            xml.getElementsByTagName("svrl:failed-assert")
+          );
+          xmlTestResults.forEach((xmlTestResult, index) => {
+            let xmlMessages = Array.from(
+              xmlTestResult.getElementsByTagName("svrl:text")
+            );
+            let messages = xmlMessages.map(
+              (xmlMessage) => xmlMessage.textContent
+            );
+            messages.push("");
+            messages.push("At location:");
+            messages = messages.concat(
+              xmlTestResult.getAttribute("location").split("/*:")
+            );
+            let testResult = {
+              spec: module.name,
+              section: part.name,
+              test: `Error #${index + 1} ` + xmlTestResult.getAttribute("test"),
+              messages: messages,
+              state: "FAIL",
+            };
+            if (!part.test) part.test = [];
+            part.test.push(testResult);
+          });
+        });
+      });
+    });
+    return result;
   }
 
   let instance = {
