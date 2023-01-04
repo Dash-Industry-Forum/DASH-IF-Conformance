2bddcb8b929417662a708bf653f4158f52190962
Daniel Silhavy <daniel.silhavy@fokus.fraunhofer.de>
2023-01-04 11:36:09 +0100

    Adjust functional test scripts to include right dependencies

--- functional-tests/cta/functionalTest.php
+++ functional-tests/cta/functionalTest.php
@@ -16,25 +16,15 @@ $argumentParser = new DASHIF\ArgumentsParser();
 include 'Utils/sessionHandler.php';
 require 'Utils/moduleInterface.php';
 include 'Utils/moduleLogger.php';
-include 'Utils/MPDHandler.php';
-include 'Utils/functions.php';
-
-include 'Utils/Session.php';         //#Session Functions, No Direct Executable Code
-//#Document loading functions, mostly xml. Some assertion options and error initialization
-include 'Utils/Load.php';
 include 'Utils/FileOperations.php';  //#Filesystem and XML checking functions. No Direct Executable Code.
-include 'Utils/VisitorCounter.php';  //#Various Session-based functions. No Direct Executable Code.
 //#Global variables. Direct evaluation of post/session vars to define conditionals,
 //#conditional extra includes for module initialization
 include 'Utils/GlobalVariables.php';
-include 'Utils/PrettyPrint.php';     //#Pretty printing functions for terminal output. No Direct Executable Code.
 include 'Utils/segment_download.php'; //#Very large function for downloading data. No Direct Executable Code.
 include 'Utils/segment_validation.php'; //#Segment validation functions. No Direct Executable Code.
-//#Dolby validation functions. Attempt at use of objects. No Direct Executable Code.
-include 'Utils/DolbySegmentValidation.php';
-
-
 include 'Utils/MPDUtility.php';
+include 'Utils/MPDHandler.php';
+include 'Utils/functions.php';
 
 
 include 'DASH/module.php';
@@ -47,16 +37,10 @@ include 'DASH/IOP/module.php';
 $argumentParser->parseAll();
 
 include 'DASH/processMPD.php';
-include 'DASH/MPDFeatures.php';
-include 'DASH/validateMPD.php';
-include 'DASH/MPDInfo.php';
 include 'DASH/SchematronIssuesAnalyzer.php';
 include 'DASH/cross_validation.php';
 include 'DASH/Representation.php';
-include 'DASH/SegmentURLs.php';
 include 'HLS/HLSProcessing.php';
-include 'Conformance-Frontend/Featurelist.php';
-include 'Conformance-Frontend/TabulateResults.php';
 
 
 set_time_limit(0);
@@ -104,14 +88,7 @@ final class functionalTest extends TestCase
         $i = 0;
         $limit = 2000;
         $startnumber = 0;
-        $blacklist = [
-            "https://dash.akamaized.net/WAVE/vectors/avc_sets/12.5_25_50/t16/2022-01-17/stream.mpd",
-            "https://dash.akamaized.net/WAVE/vectors/avc_sets/14.985_29.97_59.94/t16/2022-01-17/stream.mpd",
-            "https://dash.akamaized.net/WAVE/vectors/avc_sets/12.5_25_50/t3/2022-01-17/stream.mpd",
-            "https://dash.akamaized.net/WAVE/vectors/avc_sets/14.985_29.97_59.94/t3/2022-01-17/stream.mpd",
-            "https://dash.akamaized.net/WAVE/vectors/avc_sets/15_30_60/t16/2022-01-17/stream.mpd",
-            "https://dash.akamaized.net/WAVE/vectors/avc_sets/15_30_60/t3/2022-01-17/stream.mpd"
-        ];
+        $blacklist = [];
         $content = file_get_contents(
             "functional-tests/cta/wave.json");
         $dbJson = json_decode($content);
