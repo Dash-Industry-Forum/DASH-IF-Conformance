b5ad72b7fe1f0133446ff160e41f0d32561ace8f
Daniel Silhavy <daniel.silhavy@fokus.fraunhofer.de>
2023-01-25 13:00:27 +0100

    Adjust functional test scripts to include right dependencies (#629)
    * Adjust functional test scripts to include right dependencies
    
    * Exclude irrelevant folders from coverage report
    
    * Include missing dependencies

--- functional-tests/dashif/functionalTest.php
+++ functional-tests/dashif/functionalTest.php
@@ -16,26 +16,17 @@ $argumentParser = new DASHIF\ArgumentsParser();
 include 'Utils/sessionHandler.php';
 require 'Utils/moduleInterface.php';
 include 'Utils/moduleLogger.php';
-include 'Utils/mpdHandler.php';
-include 'Utils/functions.php';
-
-include 'Utils/Session.php';         //#Session Functions, No Direct Executable Code
-//#Document loading functions, mostly xml. Some assertion options and error initialization
-include 'Utils/Load.php';
 include 'Utils/FileOperations.php';  //#Filesystem and XML checking functions. No Direct Executable Code.
-include 'Utils/VisitorCounter.php';  //#Various Session-based functions. No Direct Executable Code.
 //#Global variables. Direct evaluation of post/session vars to define conditionals,
 //#conditional extra includes for module initialization
 include 'Utils/GlobalVariables.php';
-include 'Utils/PrettyPrint.php';     //#Pretty printing functions for terminal output. No Direct Executable Code.
 include 'Utils/segment_download.php'; //#Very large function for downloading data. No Direct Executable Code.
 include 'Utils/segment_validation.php'; //#Segment validation functions. No Direct Executable Code.
-//#Dolby validation functions. Attempt at use of objects. No Direct Executable Code.
-include 'Utils/DolbySegmentValidation.php';
-
-
 include 'Utils/MPDUtility.php';
+include 'Utils/MPDHandler.php';
+include 'Utils/functions.php';
 
+include 'Conformance-Frontend/Featurelist.php';
 
 include 'DASH/module.php';
 include 'CMAF/module.php';
@@ -47,16 +38,10 @@ include 'DASH/IOP/module.php';
 $argumentParser->parseAll();
 
 include 'DASH/processMPD.php';
-include 'DASH/MPDFeatures.php';
-include 'DASH/validateMPD.php';
-include 'DASH/MPDInfo.php';
 include 'DASH/SchematronIssuesAnalyzer.php';
 include 'DASH/cross_validation.php';
 include 'DASH/Representation.php';
-include 'DASH/SegmentURLs.php';
 include 'HLS/HLSProcessing.php';
-include 'Conformance-Frontend/Featurelist.php';
-include 'Conformance-Frontend/TabulateResults.php';
 
 
 set_time_limit(0);
@@ -102,21 +87,9 @@ final class functionalTest extends TestCase
     public function streamProvider()
     {
         $i = 0;
-        $limit = 2;
+        $limit = 2000;
         $startnumber = 0;
-        $blacklist = [
-            "https://media.axprod.net/TestVectors/v7-MultiDRM-MultiKey/Manifest_1080p.mpd",
-            "https://dash.akamaized.net/WAVE/vectors/avc_sets/14.985_29.97_59.94/t3/2022-01-17/stream.mpd",
-            "https://livesim.dashif.org/livesim/periods_60/mpdcallback_30/testpic_2s/Manifest.mpd",
-            "https://dash.akamaized.net/dash264/TestAdvertising/CMS/Axinom-CMS_HEVC_MultiRes_MultiRate_25fps.mpd",
-            "https://dash.akamaized.net/dash264/TestCasesHD/1c/qualcomm/1/MultiRate.mpd",
-            "https://dash.akamaized.net/dash264/TestCasesIOP33/adapatationSetSwitching/4/manifest.mpd",
-            "https://dash.akamaized.net/dash264/TestCases/9c/qualcomm/1/MultiRate.mpd",
-            "https://dash.akamaized.net/dash264/TestCasesHD/2c/qualcomm/1/MultiResMPEG2.mpd",
-            "https://livesim.dashif.org/livesim/periods_20/testpic_2s/Manifest.mpd",
-            "https://livesim.dashif.org/livesim/testpic_2s/Manifest.mpd#t=posix:now",
-            "https://dash.akamaized.net/dash264/TestCasesIOP33/MPDChaining/fallback_chain/2/manifest_terminationEvent_fallback_MPDChaining.mpd"
-        ];
+        $blacklist = [];
         $content = file_get_contents(
             "functional-tests/dashif/dashjs.json");
         $dbJson = json_decode($content);
--- validation-tests/dashif/validationTest.php
+++ validation-tests/dashif/validationTest.php
@@ -16,23 +16,15 @@ $argumentParser = new DASHIF\ArgumentsParser();
 include 'Utils/sessionHandler.php';
 require 'Utils/moduleInterface.php';
 include 'Utils/moduleLogger.php';
-
-include 'Utils/Session.php';         //#Session Functions, No Direct Executable Code
-//#Document loading functions, mostly xml. Some assertion options and error initialization
-include 'Utils/Load.php';
 include 'Utils/FileOperations.php';  //#Filesystem and XML checking functions. No Direct Executable Code.
-include 'Utils/VisitorCounter.php';  //#Various Session-based functions. No Direct Executable Code.
 //#Global variables. Direct evaluation of post/session vars to define conditionals,
 //#conditional extra includes for module initialization
 include 'Utils/GlobalVariables.php';
-include 'Utils/PrettyPrint.php';     //#Pretty printing functions for terminal output. No Direct Executable Code.
 include 'Utils/segment_download.php'; //#Very large function for downloading data. No Direct Executable Code.
 include 'Utils/segment_validation.php'; //#Segment validation functions. No Direct Executable Code.
-//#Dolby validation functions. Attempt at use of objects. No Direct Executable Code.
-include 'Utils/DolbySegmentValidation.php';
-
-
 include 'Utils/MPDUtility.php';
+include 'Utils/MPDHandler.php';
+include 'Utils/functions.php';
 
 
 include 'DASH/module.php';
@@ -45,16 +37,10 @@ include 'DASH/IOP/module.php';
 $argumentParser->parseAll();
 
 include 'DASH/processMPD.php';
-include 'DASH/MPDFeatures.php';
-include 'DASH/validateMPD.php';
-include 'DASH/MPDInfo.php';
 include 'DASH/SchematronIssuesAnalyzer.php';
 include 'DASH/cross_validation.php';
 include 'DASH/Representation.php';
-include 'DASH/SegmentURLs.php';
 include 'HLS/HLSProcessing.php';
-include 'Conformance-Frontend/Featurelist.php';
-include 'Conformance-Frontend/TabulateResults.php';
 
 
 set_time_limit(0);
