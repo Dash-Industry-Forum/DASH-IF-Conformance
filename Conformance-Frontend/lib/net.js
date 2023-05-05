53031ae3677576367aff6a014920c530db9ffa9c
Carlos Bentzen <carlos.bentzen@bitmovin.com>
2023-05-05 09:40:50 +0200

    use URL instatiation to validate url string

--- Conformance-Frontend/lib/net.js
+++ Conformance-Frontend/lib/net.js
@@ -38,18 +38,13 @@ const Net = (function () {
     form.submit();
   }
 
-  // https://www.freecodecamp.org/news/check-if-a-javascript-string-is-a-url/
   function isValidUrl(urlString) {
-    let urlPattern = new RegExp(
-      "^(https?:\\/\\/)?" + // validate protocol
-        "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // validate domain name
-        "((\\d{1,3}\\.){3}\\d{1,3}))" + // validate OR ip (v4) address
-        "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // validate port and path
-        "(\\?[;&a-z\\d%_.~+=-]*)?" + // validate query string
-        "(\\#[-a-z\\d_]*)?$",
-      "i"
-    ); // validate fragment locator
-    return !!urlPattern.test(urlString);
+    try {
+      const url = new URL(urlString);
+      return url.protocol === "http:" || url.protocol === "https:";
+    } catch (_) {
+      return false;
+    }
   }
 
   let instance = {
