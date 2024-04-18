5e433f25f84d1ac1df683eeb4097e7953a78a89e
Phencys <phencys@phencys.com>
2024-04-18 12:26:14 +0200

    Update framerate call in ctawave module

--- CTAWAVE/impl/checkFrameRateSplicePoint.php
+++ CTAWAVE/impl/checkFrameRateSplicePoint.php
@@ -1,42 +1,34 @@
 <?php
 
-global $session, $MediaProfDatabase, $logger;
+global $session, $MediaProfDatabase, $logger, $mpdHandler;
 
 $periodCount = sizeof($MediaProfDatabase);
 $adaptationCount = sizeof($MediaProfDatabase[0]);
 for ($i = 0; $i < ($periodCount - 1); $i++) {
     for ($adapt = 0; $adapt < $adaptationCount; $adapt++) {
-        $dir1 = $session->getRepresentationDir($i, $adapt, 0);
-        $xml1 = DASHIF\Utility\parseDOM($dir1 . '/atomInfo.xml', 'atomlist');
-        if ($xml1) {
-            $hdlr = $xml1->getElementsByTagName("hdlr")->item(0)->getAttribute("handler_type");
-            if ($hdlr == "vide") {
-                $framerate_p1 = $this->getFrameRate($xml1);
-            }
+        if ($mpdHandler->getContentType($i, $adapt) != "video") {
+            continue;
         }
-        $dir2 = $session->getRepresentationDir($i + 1, $adapt, 0);
-        $xml2 = DASHIF\Utility\parseDOM($dir2 . '/atomInfo.xml', 'atomlist');
-        if ($xml2) {
-            $hdlr = $xml2->getElementsByTagName("hdlr")->item(0)->getAttribute("handler_type");
-            if ($hdlr == "vide") {
-                $framerate_p2 = $this->getFrameRate($xml2);
-
-
-                $remainder = ($framerate_p1 > $framerate_p2 ?
-                  ($framerate_p1 % $framerate_p2) :
-                  ($framerate_p2 % $framerate_p1));
-
-
-                $logger->test(
-                    "WAVE Content Spec 2018Ed",
-                    "Section 7.2.2",
-                    "Frame rate Should be the same between Sequential Sw Sets at the Splice point",
-                    $remainder == 0,
-                    "WARN",
-                    "Correct for Sw set $adapt between presentations $i and " . ($i + 1),
-                    "Invalid for Sw set $adapt between presentations $i and " . ($i + 1),
-                );
-            }
+        if ($mpdHandler->getContentType($i + 1, $adapt) != "video") {
+            continue;
         }
+        $framerate_p1 = $mpdHandler->getFrameRate($i, $adapt, 0);
+        $framerate_p2 = $mpdHandler->getFrameRate($i + 1, $adapt, 0);
+
+
+        $remainder = ($framerate_p1 > $framerate_p2 ?
+          ($framerate_p1 % $framerate_p2) :
+          ($framerate_p2 % $framerate_p1));
+
+
+        $logger->test(
+            "WAVE Content Spec 2018Ed",
+            "Section 7.2.2",
+            "Frame rate Should be the same between Sequential Sw Sets at the Splice point",
+            $remainder == 0,
+            "WARN",
+            "Correct for Sw set $adapt between presentations $i and " . ($i + 1),
+            "Invalid for Sw set $adapt between presentations $i and " . ($i + 1),
+        );
     }
 }
