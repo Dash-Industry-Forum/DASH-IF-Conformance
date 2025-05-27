b71937704e0385dd8b7f22960980384e23d5e44c
Erik Zandvliet <phencys@phencys.com>
2025-05-27 16:10:04 +0200

    Revise codingstyle in helper functions

--- jccp/app/Helpers/utility.php
+++ jccp/app/Helpers/utility.php
@@ -22,7 +22,9 @@ if (! function_exists('formSegmentAccess')) {
             foreach ($highValue as $k => $v) {
                 if (!array_key_exists($k, $lowValue) || !$lowValue[$k]) {
                     $lowValue[$k] = $v;
-                } elseif (gettype($lowValue[$k]) == 'array') {
+                    continue;
+                }
+                if (gettype($lowValue[$k]) == 'array') {
                     //$v would also work, but this is more clear in meaning
                     $lowValue[$k] = formSegmentAccess($highValue[$k], $lowValue[$k]);
                 }
@@ -61,61 +63,61 @@ if (! function_exists('isAbsoluteURL')) {
 
 
 if (! function_exists('timeParsing')) {
-    function timeParsing(string $var): int
+    function timeParsing(string $input): int
     {
-        $y = str_replace("P", "", $var);
-        if (strpos($y, 'Y') !== false) { // Year
-            $Y = explode("Y", $y);
-            $y = substr($y, strpos($y, 'Y') + 1);
+        $remainder = str_replace("P", "", $input);
+        if (strpos($remainder, 'Y') !== false) { // Year
+            $year = intval(explode("Y", $remainder)[0]);
+            $remainder = substr($remainder, strpos($remainder, 'Y') + 1);
         } else {
-            $Y[0] = 0;
+            $year = 0;
         }
 
-        if (strpos($y, 'M') !== false && strpos($y, 'M') < strpos($y, 'T')) { // Month
-            $Mo = explode("M", $y);
-            $y = substr($y, strpos($y, 'M') + 1);
+        if (strpos($remainder, 'M') !== false && strpos($remainder, 'M') < strpos($remainder, 'T')) { // Month
+            $month = intval(explode("M", $remainder)[0]);
+            $remainder = substr($remainder, strpos($remainder, 'M') + 1);
         } else {
-            $Mo[0] = 0;
+            $month = 0;
         }
 
-        if (strpos($y, 'W') !== false) { // Week
-            $W = explode("W", $y);
-            $y = substr($y, strpos($y, 'W') + 1);
+        if (strpos($remainder, 'W') !== false) { // Week
+            $week = intval(explode("W", $remainder)[0]);
+            $remainder = substr($remainder, strpos($remainder, 'W') + 1);
         } else {
-            $W[0] = 0;
+            $week = 0;
         }
 
-        if (strpos($y, 'D') !== false) { // Day
-            $D = explode("D", $y);
-            $y = substr($y, strpos($y, 'D') + 1);
+        if (strpos($remainder, 'D') !== false) { // Day
+            $day = intval(explode("D", $remainder)[0]);
+            $remainder = substr($remainder, strpos($remainder, 'D') + 1);
         } else {
-            $D[0] = 0;
+            $day = 0;
         }
 
-        $y = str_replace("T", "", $y);
-        if (strpos($y, 'H') !== false) { // Hour
-            $H = explode("H", $y);
-            $y = substr($y, strpos($y, 'H') + 1);
+        $remainder = str_replace("T", "", $remainder);
+        if (strpos($remainder, 'H') !== false) { // Hour
+            $hours = intval(explode("H", $remainder)[0]);
+            $remainder = substr($remainder, strpos($remainder, 'H') + 1);
         } else {
-            $H[0] = 0;
+            $hours = 0;
         }
 
-        if (strpos($y, 'M') !== false) { // Minute
-            $M = explode("M", $y);
-            $y = substr($y, strpos($y, 'M') + 1);
+        if (strpos($remainder, 'M') !== false) { // Minute
+            $minutes = intval(explode("M", $remainder)[0]);
+            $remainder = substr($remainder, strpos($remainder, 'M') + 1);
         } else {
-            $M[0] = 0;
+            $minutes = 0;
         }
 
-        $S = explode("S", $y); // Second
+        $seconds = intval(explode("S", $remainder)[0]);
 
-///\todo This function does not take into account leap years. There has to be a better way for this?
-        return (intval($Y[0]) * 365 * 24 * 60 * 60) +
-        (intval($Mo[0]) * 30 * 24 * 60 * 60) +
-        (intval($W[0]) * 7 * 24 * 60 * 60) +
-        (intval($D[0]) * 24 * 60 * 60) +
-        (intval($H[0]) * 60 * 60) +
-        (intval($M[0]) * 60) +
-        intval($S[0]); // calculate durations in seconds
+        ///\todo This function does not take into account leap years. There has to be a better way for this?
+        return ($year * 365 * 24 * 60 * 60) +
+        ($month * 30 * 24 * 60 * 60) +
+        ($week * 7 * 24 * 60 * 60) +
+        ($day * 24 * 60 * 60) +
+        ($hours * 60 * 60) +
+        ($minutes * 60) +
+        $seconds;
     }
 }
