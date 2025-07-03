dc3c825d88e114244c1ddc0af50c1f44e167dda1
Erik Zandvliet <phencys@phencys.com>
2025-07-03 14:08:17 +0200

    Cleanup of no-longer used elements

--- jccp/bootstrap/providers.php
+++ jccp/bootstrap/providers.php
@@ -1,11 +1,9 @@
 <?php
 
 return [
-    App\Providers\AppServiceProvider::class,
     App\Providers\MPDCacheProvider::class,
     App\Providers\ModuleLoggerProvider::class,
     App\Providers\ModuleReporterProvider::class,
     App\Providers\ValidatorProvider::class,
     App\Providers\DownloaderProvider::class,
-
 ];
