f695fff41e135da114d2bc1547b1da64b7b246c6
Erik Zandvliet <phencys@phencys.com>
2025-07-07 11:03:09 +0200

    TLS Bitrate legacy check

--- jccp/app/Modules/DVB/MPD.php
+++ jccp/app/Modules/DVB/MPD.php
@@ -5,33 +5,48 @@ namespace App\Modules\DVB;
 use App\Services\ModuleLogger;
 use App\Services\MPDCache;
 use App\Services\ModuleReporter;
+use App\Services\Reporter\SubReporter;
 use App\Services\Reporter\Context as ReporterContext;
 use App\Interfaces\Module;
+use App\Modules\DVB\TLSBitrate;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Cache;
 
 class MPD extends Module
 {
+    private SubReporter $v141reporter;
+    private SubReporter $legacyreporter;
+
     public function __construct()
     {
         parent::__construct();
         $this->name = "DASH-IF IOP Conformance";
+
+        $reporter = app(ModuleReporter::class);
+
+        $this->legacyreporter = &$reporter->context(new ReporterContext(
+            "MPD",
+            "DVB",
+            "LEGACY",
+            []
+        ));
+        $this->v141reporter = &$reporter->context(new ReporterContext(
+            "MPD",
+            "DVB",
+            "v1.4.1",
+            ["document" => "ETSI TS 103 285"]
+        ));
     }
 
     public function validateMPD(): void
     {
         parent::validateMPD();
-        $logger = app(ModuleLogger::class);
-        $reporter = app(ModuleReporter::class);
-
         $mpdCache = app(MPDCache::class);
 
+        ///TODO Make this a remember function
         $resolved = Cache::get(cache_path(['mpd', 'resolved']));
 
-        $v141context = new ReporterContext("MPD", "DVB", "v1.4.1", ["document" => "ETSI TS 103 285"]);
-        $v141reporter = &$reporter->context($v141context);
-
-        $v141reporter->test(
+        $this->v141reporter->test(
             "Section 4.5",
             "The MPD size after xlink resolution SHALL NOT exceed 256 Kbytes",
             $resolved && strlen($resolved) <= 1024 * 256,
@@ -40,14 +55,17 @@ class MPD extends Module
             ($resolved ? "MPD too large" : "No resolved MPD found")
         );
 
-        $logger->test(
-            "DVB",
-            "Section 4.5",
-            "The MPD size after xlink resolution SHALL NOT exceed 256 Kbytes",
-            $resolved && strlen($resolved) <= 1024 * 256,
-            "FAIL",
-            "MPD Size in bounds",
-            ($resolved ? "MPD too large" : "No resolved MPD found")
+        $minimumUpdatePeriod = $mpdCache->getAttribute('minimumUpdatePeriod');
+
+        $this->legacyreporter->test(
+            "Unknown",
+            "MPD@minimumUpdatePeriod SHOULD have a value of 1 second or higher",
+            ($minimumUpdatePeriod != '' && timeParsing($minimumUpdatePeriod) < 1),
+            "WARN",
+            "Check succeeded",
+            "Check failed"
         );
+
+        new TLSBitrate()->validateTLSBitrate();
     }
 }
---
+++ jccp/app/Modules/DVB/TLSBitrate.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Modules\DVB;
+
+use App\Services\MPDCache;
+use App\Services\Manifest\Period;
+use App\Services\ModuleReporter;
+use App\Services\Reporter\SubReporter;
+use App\Services\Reporter\Context as ReporterContext;
+use App\Interfaces\Module;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Cache;
+
+class TLSBitrate
+{
+    private SubReporter $legacyreporter;
+
+    public function __construct()
+    {
+        $reporter = app(ModuleReporter::class);
+        $this->legacyreporter = &$reporter->context(new ReporterContext(
+            "MPD",
+            "DVB",
+            "LEGACY",
+            []
+        ));
+    }
+
+    public function validateTLSBitrate(): void
+    {
+        $mpdCache = app(MPDCache::class);
+
+        //Only applicable for TLS streams
+        if (!str_starts_with($mpdCache->getBaseUrl(), "https://")) {
+            return;
+        }
+
+        foreach ($mpdCache->allPeriods() as $period) {
+            $this->constraintsByPeriod($period);
+        }
+    }
+
+    /**
+     * @return array<string, array<string, int>>
+     **/
+    private function extractBandwidths(Period $period): array
+    {
+        $res = [
+            'video' => [],
+            'audio' => [],
+            'subtitle' => []
+        ];
+
+        foreach ($period->allAdaptationSets() as $adaptationSet) {
+            foreach ($adaptationSet->allRepresentations() as $representation) {
+                $representationId = $representation->getAttribute('id');
+                $representationBandwith = $representation->getAttribute('bandwidth');
+                $context = '';
+                switch ($representation->getTransientAttribute('mimeType')) {
+                    case 'video/mp4':
+                        $context = 'video';
+                        break;
+                    case 'audio/mp4':
+                        $context = 'audio';
+                        break;
+                    case 'application/mp4':
+                        $context = 'subtitle';
+                        break;
+                }
+                if ($context != '') {
+                    $res[$context][$representationId] = $representationBandwith;
+                }
+            }
+        }
+
+        foreach (['video', 'audio','subtitle'] as $context) {
+            if (count($res[$context]) == 0) {
+                $res[$context]["No $context"] = 0;
+            }
+        }
+
+        return $res;
+    }
+
+    private function constraintsByPeriod(Period $period): void
+    {
+        //Check if any combination excedes the constraint
+
+        $bandwidth = $this->extractBandwidths($period);
+
+        foreach ($bandwidth['video'] as $vRepId => $vRepBandwidth) {
+            foreach ($bandwidth['audio'] as $aRepId => $aRepBandwidth) {
+                foreach ($bandwidth['subtitle'] as $sRepId => $sRepBandwidth) {
+                    $totalBandwidth = $vRepBandwidth + $aRepBandwidth + $sRepBandwidth;
+
+                    $bandWidthMessage = implode(", ", [
+                        "V@" . number_format($vRepBandwidth / 1000000, 2) . "Mbit/s",
+                        "A@" . number_format($aRepBandwidth / 1000000, 2) . "Mbit/s",
+                        "S@" . number_format($sRepBandwidth / 1000000, 2) . "Mbit/s"
+                    ]);
+                    $totalBandWidthMessage = "Total: " . number_format($totalBandwidth / 1000000, 2) . "Mbit/s";
+
+                    $combinationMessage = implode(", ", [
+                        "V:" . $vRepId,
+                        "A:" . $aRepId,
+                        "S:" . $sRepId
+                    ]);
+
+                    $msgPrefix = "Period " . $period->path() . " ($combinationMessage)";
+                    $inBoundsMessage = "$msgPrefix within bounds: $totalBandWidthMessage: $bandWidthMessage";
+                    $exceedsMessage = "$msgPrefix exceeds bounds: $totalBandWidthMessage: $bandWidthMessage";
+
+
+                    $this->legacyreporter->test(
+                        "Unknown",
+                        "Bitrate checks for terminal that does support UHD HFR video (max 51 Mbit/s)",
+                        $totalBandwidth <= 51000000,
+                        "WARN",
+                        $inBoundsMessage,
+                        $exceedsMessage
+                    );
+                    $this->legacyreporter->test(
+                        "Unknown",
+                        "Bitrate checks for terminal that does support UHD video, but not HFR video (max 39 Mbit/s)",
+                        $totalBandwidth <= 39000000,
+                        "WARN",
+                        $inBoundsMessage,
+                        $exceedsMessage
+                    );
+                    $this->legacyreporter->test(
+                        "Unknown",
+                        "Bitrate checks for terminal that does not support UHD video (max 12 Mbit/s)",
+                        $totalBandwidth <= 12000000,
+                        "WARN",
+                        $inBoundsMessage,
+                        $exceedsMessage
+                    );
+                }
+            }
+        }
+    }
+}
