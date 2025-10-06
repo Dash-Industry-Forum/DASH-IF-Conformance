2139da754bf5138bc31a7388e5f6cf5305d8a946
Erik Zandvliet <phencys@phencys.com>
2025-10-06 09:25:30 +0200

    With trusted proxy setting

--- jccp/bootstrap/app.php
+++ jccp/bootstrap/app.php
@@ -11,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
