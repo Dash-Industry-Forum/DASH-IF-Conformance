c9e076094dac42a4db4d2c47582612c9045a08fc
Erik Zandvliet <phencys@phencys.com>
2026-01-19 10:25:27 +0100

    Revised 'about' page

--- jccp/resources/views/about.blade.php
+++ jccp/resources/views/about.blade.php
@@ -1,36 +1,34 @@
 <x-layout>
-  <x-outdated />
   <div class="container">
-  <h4>About</h4>
+    <h4 class="text-center mb-4">About</h4>
 
-  <p>
-    DASH-IF Conformance Tool is used to validate DASH content according to DASH-related media specifications. It aims to give information about the validity of the content against one or more developed media standards. Consequently, it reports on (un)expected behavior that can be observed in provided media services that are aimed to be working in alignment to these standards.
-  </p>
+    <p>
+      The DASH-IF Conformance Tool can be used to validate DASH content according to DASH-related media specifications. It aims to give information about the validity of the content against one or more developed media standards. Consequently, it reports on (un)expected behavior that can be observed in provided media services that are aimed to be working in alignment to these standards.
+    </p>
 
-  <p>
-The development of the tool started in 2012 funded by DASH-IF and has been continuously updated with newer versions of the already supported standards and/or new standards as required. Currently, the tool is aligned with a large set of specifications, namely MPEG-DASH,ISO BMFF, DASH-IF IOP, CMAF, DVB-DASH, HbbTV and CTA WAVE. It also integrates file format header level parsing of media codecs, including AVC, HEVC, AAC,HE-AAC, HE-AACv2, AC-3, AC-4, E-AC-3, WebVTT and TTML.
-  </p>
-
-
-  <p>
-    For each corresponding specification, the scope of the validation covers:
-  </p>
+    <p>
+  The development of the tool started in 2012, funded by DASH-IF and has since been updated with newer versions of the already supported standards and/or new standards as required. Currently, the tool is aligned with a large set of specifications, and also integrates file format header level parsing of several media codecs.
+    </p>
 
 
-  <ul>
-    <li>
-      <em>Media PresentationDescription (MPD)</em> validation where the MPD is checked if it is a well-formed XML file, appropriate according to DASH schema and MPD-level signaling is done correctly
-    </li>
-    <li>
-      <em>Segment validation</em> where the media content pointed to by the MPD is validated at container level,
-    </li>
-    <li>
-      <em>Cross validation</em> of the MPD-level elements and attributes as well as of the mediacontent(s) signaled at the same hierarchy.
-    </li>
-  </ul>
+    <p>
+      Three categories of validation are covered, depending on the specification:
+    </p>
 
-  <p>
-The DASH-IF Conformance Tool is an open source software available on <a href="https://github.com/Dash-Industry-Forum/DASH-IF-Conformance" target="_blank">Github</a>. A live demo of the tool is also provided <a href="https://conformance.dashif.org/" target="_blank">here</a>.
-</p>
+
+    <ul>
+      <li><em>MPD (Media Presentation Description) validation</em>: Validates well-formed XML files against the MPD specification, as well as checks various requirements and assumptions.
+      </li>
+      <li>
+        <em>Segment validation</em>: Validates the corresponding media segments as refered to by the MPD.
+      </li>
+      <li>
+        <em>Cross validation</em>: Validates the data present in the MPD against the data provided by analysing the Segments.
+      </li>
+    </ul>
+
+    <p>
+      The DASH-IF Conformance Tool is an open source software available on <a href="https://github.com/Dash-Industry-Forum/DASH-IF-Conformance" target="_blank">Github</a>. A live demo of the tool is also provided <a href="https://conformance.dashif.org/" target="_blank">here</a>.
+    </p>
   </div>
 </x-layout>
