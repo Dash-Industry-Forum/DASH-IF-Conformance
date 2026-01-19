a067e0a9f9afc9c0543cf3c2015e6c361c86793f
Erik Zandvliet <phencys@phencys.com>
2026-01-19 11:45:27 +0100

    Reworded the consent message

--- jccp/resources/views/livewire/consent-manager.blade.php
+++ jccp/resources/views/livewire/consent-manager.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
       <div class="row align-items-center">
         <div class="col-11">
-          <b>You have given us permission to collect usage data in order to improve the tool</b>
+          <b>You have given us permission to collect usage data in order to improve the application.</b>
         </div>
         <button class="col-1 btn btn-outline-danger" type="button" wire:click="revoke">Revoke</button>
       </div>
@@ -16,7 +16,7 @@
       <div class="row align-items-center">
         <button class="btn btn-outline-success col-1" type="button" wire:click="accept">Accept</button>
         <div class="col-11">
-          <b>We collect usage data in order to improve the tool</b>
+          <b>We collect usage data in order to improve the application.</b>
           <div>
             If you work with private data, consider <a target="_blank" href="https://github.com/Dash-Industry-Forum/DASH-IF-Conformance/wiki/Installation--guide">Self Hosting</a> instead.
           </div>
